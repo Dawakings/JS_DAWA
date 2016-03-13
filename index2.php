8a69482baaf035132c6d2eef212cb4db31a12bd2
robert <robert@robert-X501A>
2016-03-13 19:07:33 +0100

    kundvagn ej klar - annat är najs

--- index2.php
+++ index2.php
@@ -5,7 +5,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 //inkluderar
 include_once 'Controller.php';
-//surfar till denna sida med länken index2.php?getAllCars
+//surfar till denna sida med länken index2.php/getAllavaror
 //delar upp querystringen till en array med /som avskiljare
 //arrayens innehåll:
 //$queryArray[0]=getAllCars
@@ -14,7 +14,7 @@ $queryArray = explode('/', $_SERVER['PATH_INFO']);
 //instansierar ett nytt controller objekt
 $cont = new Controller();
 //anropar metod på controllerobjekt
-//blir tex $cont->getAllCars();
+//blir tex $cont->getAllavaror();
 
 
 
@@ -22,13 +22,13 @@ if (method_exists($cont, $queryArray[1])) {
     
     switch ($queryArray[1]) {
         case 'getAllavaror':
-            echo json_encode($cont->$queryArray[1]());
+            echo json_encode($cont->$queryArray[1]()); //Visar JSON-encoded-string. Läses av JS-funktioner.
             break;
         case 'getKategories':
-            echo json_encode($cont->$queryArray[1]());
+            echo json_encode($cont->$queryArray[1]()); // utan argument
             break;
         case 'getinfoByKategori':
-            echo json_encode($cont->$queryArray[1]($queryArray[2]));
+            echo json_encode($cont->$queryArray[1]($queryArray[2])); //metod(argument). Ex getinfoByKategori/Datorer
             break;
         case 'getinfo':
             echo json_encode($cont->$queryArray[1]($queryArray[2]));
